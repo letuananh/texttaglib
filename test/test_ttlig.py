a0e84dad33d849d4e64ff398a895903125a5e0b9
Le Tuan Anh <tuananh.ke@gmail.com>
2020-10-20 12:00:27 +0800

    update test case

--- test/test_ttlig.py
+++ test/test_ttlig.py
@@ -47,8 +47,6 @@ from chirptext import deko
 from texttaglib import ttl
 from texttaglib import ttlig
 from texttaglib.ttlig import IGStreamReader, TTLTokensParser
-from texttaglib.ttlig import Transcript
-from texttaglib.vtt import sec2ts
 
 
 # -------------------------------------------------------------------------------
@@ -85,7 +83,7 @@ class TestTokenizer(unittest.TestCase):
         for tk, gl in zip(sent.tokens, glosses):
             tk.new_tag(gl, tagtype='gloss')
         # verify imported information
-        actual = [(t.text, t.find('gloss').label) for t in sent]
+        actual = [(t.text, t.get_tag('gloss').label) for t in sent]
         expected = [('It', 'SUBJ'), ('works', 'work'), ('.', 'PUNC')]
         self.assertEqual(expected, actual)
 
@@ -267,17 +265,6 @@ I have two cat-s.
     def test_parsing_aligned_text(self):
         print("Testing TTLIG with multiple spaces")
 
-
-class TestTranscript(unittest.TestCase):
-
-    def test_read_transcript(self):
-        print("Test reading an ELAN transcript output file")
-        transcript = Transcript.read_elan(TRANSCRIPT_FILE)
-        timeline = transcript.sort()  # sort by time
-        human_readble = [[x.tier, sec2ts(x.tsfrom), sec2ts(x.tsto), sec2ts(x.tsduration), x.text] for x in timeline]
-        getLogger().debug('Human readble format:\n' + '\n'.join('\t'.join(x) for x in human_readble))
-        expected = chio.read_tsv(TRANSCRIPT_EXPECTED_FILE)
-        self.assertEqual(human_readble, expected)
 
 # -------------------------------------------------------------------------------
 # MAIN
