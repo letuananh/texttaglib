2f66a2994641de6c16bf0e44a9f5445927ce882a
Le Tuan Anh <tuananh.ke@gmail.com>
2021-03-29 16:28:30 +0800

    bump version to 0.1a8

--- texttaglib/__version__.py
+++ texttaglib/__version__.py
@@ -10,6 +10,6 @@ __description__ = "Python library for managing and annotating textual corpus usi
 __url__ = "https://github.com/letuananh/texttaglib"
 __maintainer__ = "Le Tuan Anh"
 __version_major__ = "0.1"
-__version__ = "{}a7".format(__version_major__)
+__version__ = "{}a8".format(__version_major__)
 __version_long__ = "{} - Alpha".format(__version_major__)
 __status__ = "Prototype"
